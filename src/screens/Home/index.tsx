aa263ba6f93c472c55bc9ba032d16bf0a97093ff
willslaq <willslaqtomaz@gmail.com>
2021-06-22 00:27:27 -0300

    Feito Appointment, GuildIcon e ListHeader/ListDivider, para o content da Home, neste um aprendizado muito interessante foi a questão do FlatList, quando utilizar ela ou o ScrollList: FlatList carrega aos poucos conforme o uso, ou seja, mais performática quando há muitos itens na lista; Enquanto que a ScrollList traz todos de uma vez na tela, para listas grandes toma muito tempo de renderização

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,46 @@
 import React, { useState } from "react";
-import { View } from "react-native";
+import { View, FlatList } from "react-native";
 
 import { Profile } from "../../components/Profile";
 import { ButtonAdd } from "../../components/ButtonAdd";
 import { CategorySelect } from "../../components/CategorySelect";
+import { ListDivider } from "../../components/ListDivider";
+import { ListHeader } from "../../components/ListHeader";
+import { Appointment } from "../../components/Appointment";
 
 import { styles } from "./styles";
 
 export function Home() {
   const [category, setCategory] = useState("");
+
+  const appointments = [
+    {
+      id: "1",
+      guild: {
+        id: "1",
+        name: "Lendários",
+        icon: null,
+        owner: true,
+      },
+      category: "1",
+      date: "22/06 às 20:40h",
+      description:
+        "É hoje que vamos chegar ao challenger sem perder uma partida da md10",
+    },
+    {
+      id: "2",
+      guild: {
+        id: "1",
+        name: "Lendários",
+        icon: null,
+        owner: true,
+      },
+      category: "1",
+      date: "22/06 às 20:40h",
+      description:
+        "É hoje que vamos chegar ao challenger sem perder uma partida da md10",
+    },
+  ];
 
   function handleCategorySelect(categoryId: string) {
     categoryId === category ? setCategory("") : setCategory(categoryId);
@@ -24,6 +56,18 @@ export function Home() {
         categorySelected={category}
         setCategory={handleCategorySelect}
       />
+      <View style={styles.content}>
+        <ListHeader title="Partidas agendadas" subtitle="Total 6" />
+        {/* FlatList utilizar ao ter muitos itens a ser renderizado, pois a ScrollView carrega todos em tela ***Mais Performático*** */}
+        <FlatList
+          data={appointments}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <Appointment data={item} />}
+          ItemSeparatorComponent={() => <ListDivider />}
+          style={styles.matches}
+          showsHorizontalScrollIndicator={false}
+        />
+      </View>
     </View>
   );
 }
